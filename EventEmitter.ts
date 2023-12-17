f64efb6e8c27b0e46fff32baa6d355d4acd05e17
Jakub Jankiewicz <jcubic@onet.pl>
2023-12-17 17:10:28 +0100

    add EventEmitter class

---
+++ EventEmitter.ts
@@ -0,0 +1,29 @@
+export type Handler<T extends unknown[]> = (...args: T) => void;
+
+export class EventEmitter<T extends unknown[]> {
+  private handlers: {[key: string]: Array<Handler<T>>}  = {};
+  on(event: string, handler: Handler<T>) {
+    if (!this.handlers[event]) {
+      this.handlers[event] = [];
+    }
+    this.handlers[event].push(handler);
+  }
+  off(event: string, handler?: Handler<T>) {
+    if (this.handlers[event]) {
+      if (handler) {
+        this.handlers[event] = this.handlers[event].filter(fn => {
+          return handler === fn;
+        });
+      } else {
+        delete this.handlers[event];
+      }
+    }
+  }
+  trigger(event: string, args: T) {
+    if (this.handlers[event]) {
+      this.handlers[event].forEach(fn => {
+        fn(...args);
+      });
+    }
+  }
+};
